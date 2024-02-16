bd26901ddf64b2603078e7ff9b8b66c09085f04c
Mohammed Mahedi Hasan <mahediHasan7@gmail.com>
2024-02-16 14:38:02 +0800

    add read and write access policies to each endpoints

--- Endpoints/GameEndpoints.cs
+++ Endpoints/GameEndpoints.cs
@@ -1,3 +1,4 @@
+using GameStore.Api.Authorization;
 using GameStore.Api.Dto;
 using GameStore.Api.Entities;
 using GameStore.Api.Repositories;
@@ -31,7 +32,7 @@ public static class GameEndpoints
       return Results.Ok(game.AsDto());
     })
     .WithName(GetGameEndpointName)
-    .RequireAuthorization();
+    .RequireAuthorization(Policies.ReadAccess);
 
     // POST
     group.MapPost("/", async (IGameRepository repository, CreateGameDto gameDto) =>
@@ -47,7 +48,7 @@ public static class GameEndpoints
 
       await repository.CreateAsync(game);
       return Results.CreatedAtRoute(GetGameEndpointName, new { id = game.Id }, game);
-    }).RequireAuthorization();
+    }).RequireAuthorization(Policies.WriteAccess);
 
     // PUT
     group.MapPut("/{id}", async (IGameRepository repository, int id, UpdateGameDto gameDto) =>
@@ -68,7 +69,7 @@ public static class GameEndpoints
       await repository.UpdateAsync(existingGame);
 
       return Results.Ok(existingGame);
-    }).RequireAuthorization();
+    }).RequireAuthorization(Policies.WriteAccess);
 
     // DELETE
     group.MapDelete("/{id}", async (IGameRepository repository, int id) =>
@@ -82,9 +83,7 @@ public static class GameEndpoints
 
       await repository.DeleteAsync(id);
       return Results.NoContent();
-    }).RequireAuthorization();
-
-    Console.WriteLine(group.GetType().FullName);
+    }).RequireAuthorization(Policies.WriteAccess);
 
     return group;
   }
